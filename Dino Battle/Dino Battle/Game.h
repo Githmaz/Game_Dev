621537375ab79277222f5ac121e8ee2ccc10ef2c
Githmaz <Githmaz1@gmail.com>
2025-04-12 23:54:11 +0530

    add game header

---
+++ Dino Battle/Dino Battle/Game.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include "DinoManager.h"
+#include "Dinosaur.h"
+class Game
+{
+private:
+		DinoManager* dinoManager;
+		Dinosaur* userDinoList;
+		int opponentScore;
+		int roundsPlayed;
+		int totalRounds;
+		bool gameOver;
+
+public:
+}
+
+};
